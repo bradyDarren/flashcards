dc58882d7e38673303f758f8deccb218b0369876
bradyDarren <dcgbfinancial@gmail.com>
2025-03-07 22:31:49 -0600

    addition of the canvas

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from tkinter import * 
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+# ------------------------------------- UI --------------------------------
+window = Tk() 
+window.title("Spanish Flashcards")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=800, height=526, highlightthickness=0)
+card_pos = PhotoImage(file='card_front.png')
+canvas.create_image(400,263,image = card_pos)
+canvas.grid(column=0, row=0)
 
 
-BACKGROUND_COLOR = "#B1DDC6"x
+window.mainloop()
