61a8245f977ae4b13e649fa1eda1e356422d09cb
bradyDarren <dcgbfinancial@gmail.com>
2025-03-08 14:19:00 -0600

    addition of the text within the canvas

--- main.py
+++ main.py
@@ -10,13 +10,9 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, highlightthickness=0,bg=BACKGROUND_COLOR)
 card_pos = PhotoImage(file='images/card_front.png')
 canvas.create_image(400,263,image = card_pos)
+canvas.create_text(400, 150,text='Title', font=('Ariel', 40,'italic'), fill='black')
+canvas.create_text(400, 263, text='word', font=('Ariel', 60,'bold'), fill='black')
 canvas.grid(column=0, row=0, columnspan=2)
- 
-#Labels
-language = Label(text='Title', font=('Ariel', 40,'italic'), bg='white',fg='black')
-language.place(x=310,y=150)
-word = Label(text='word', font=('Ariel', 60, 'bold'), fg='black', bg='white')
-word.place(x=290, y=263)
 
 # Buttons
 wrong_image = PhotoImage(file="images/wrong.png")
