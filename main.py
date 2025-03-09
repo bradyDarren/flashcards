c8b3c0f703c08e3c91bf9c7fc232cff609297c07
bradyDarren <dcgbfinancial@gmail.com>
2025-03-09 15:02:02 -0500

    moving of the random word generation to the top of the code

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import pandas as pd
 BACKGROUND_COLOR = "#B1DDC6"
 # ------------------ Random Word Generation ---------------------------------
 spanish = pd.read_csv(filepath_or_buffer='data/spanish.csv')
-data_dict = spanish.to_dict(orient='records')
+to_learn = spanish.to_dict(orient='records')
 
 def new_word():
-    rand_word = r.choice(data_dict)['Spanish Word']
-    canvas.itemconfig(word, text=rand_word)
+    new_card = r.choice(to_learn)['Spanish Word']
+    canvas.itemconfig(card_word, text=new_card)
+    canvas.itemconfig(card_title, text= "Spanish")
 
 # ---------------------------- UI -------------------------------------------
 window = Tk() 
@@ -17,12 +18,11 @@ window.title("Spanish Flashcards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # -------------------------- Canvas ------------------------------------------
-beg_word = r.choice(data_dict)['Spanish Word']
 canvas = Canvas(width=800, height=526, highlightthickness=0,bg=BACKGROUND_COLOR)
 card_pos = PhotoImage(file='images/card_front.png')
 canvas.create_image(400,263,image = card_pos)
-title = canvas.create_text(400, 150,text='Spanish', font=('Ariel', 40,'italic'), fill='black')
-word = canvas.create_text(400, 263, text=beg_word, font=('Ariel', 60,'bold'), fill='black')
+card_title = canvas.create_text(400, 150,text='', font=('Ariel', 40,'italic'), fill='black')
+card_word = canvas.create_text(400, 263, text='', font=('Ariel', 60,'bold'), fill='black')
 canvas.grid(column=0, row=0, columnspan=2)
 
 # -------------------------- Buttons ------------------------------------------
@@ -34,5 +34,6 @@ known_image = PhotoImage(file="images/right.png")
 known_button = Button(image=known_image, borderwidth=0, relief='flat', highlightthickness=0, command=new_word)
 known_button.grid(column=1, row=1)
 
+new_word()
 
 window.mainloop()
