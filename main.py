cf5a0de06e740eb76ea23a403e2e12507240c195
bradyDarren <dcgbfinancial@gmail.com>
2025-03-08 22:40:18 -0600

    made buttons functional. when clicked they will generate a new word

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-from tkinter import * 
+from tkinter import *
+import random as r 
+import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
-# ------------------------------------- UI --------------------------------
+# ------------------ Random Word Generation ---------------------------------
+spanish = pd.read_csv(filepath_or_buffer='data/spanish.csv')
+data_dict = spanish.to_dict(orient='records')
+
+def new_word():
+    rand_word = r.choice(data_dict)['Spanish Word']
+    canvas.itemconfig(word, text=rand_word)
+
+# ---------------------------- UI -------------------------------------------
 window = Tk() 
 window.title("Spanish Flashcards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-# Canvas
+# -------------------------- Canvas ------------------------------------------
 canvas = Canvas(width=800, height=526, highlightthickness=0,bg=BACKGROUND_COLOR)
 card_pos = PhotoImage(file='images/card_front.png')
 canvas.create_image(400,263,image = card_pos)
-canvas.create_text(400, 150,text='Title', font=('Ariel', 40,'italic'), fill='black')
-canvas.create_text(400, 263, text='word', font=('Ariel', 60,'bold'), fill='black')
+title = canvas.create_text(400, 150,text='Spanish', font=('Ariel', 40,'italic'), fill='black')
+word = canvas.create_text(400, 263, text='word', font=('Ariel', 60,'bold'), fill='black')
 canvas.grid(column=0, row=0, columnspan=2)
 
-# Buttons
+# -------------------------- Buttons ------------------------------------------
 unkown_image = PhotoImage(file="images/wrong.png")
-unkown_button = Button(image=unkown_image, borderwidth=0, relief='flat', highlightthickness=0)
+unkown_button = Button(image=unkown_image, borderwidth=0, relief='flat', highlightthickness=0, command=new_word)
 unkown_button.grid(column=0, row=1)
-known_image = PhotoImage(file="images/right.png")
-known_button = Button(image=known_image, borderwidth=0, relief='flat', highlightthickness=0)
-known_button.grid(column=1, row=1)
 
+known_image = PhotoImage(file="images/right.png")
+known_button = Button(image=known_image, borderwidth=0, relief='flat', highlightthickness=0, command=new_word)
+known_button.grid(column=1, row=1)
 
 
 window.mainloop()
