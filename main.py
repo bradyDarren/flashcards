d28d5b8785ade0c62bbbe464fd20e69c461f15dd
bradyDarren <dcgbfinancial@gmail.com>
2025-03-09 21:50:50 -0500

    creation of the flip card method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import random as r 
 import pandas as pd
+import time
 
 BACKGROUND_COLOR = "#B1DDC6"
 # ------------------ Random Word Generation ---------------------------------
@@ -8,9 +9,22 @@ spanish = pd.read_csv(filepath_or_buffer='data/spanish.csv')
 to_learn = spanish.to_dict(orient='records')
 
 def new_word():
-    new_card = r.choice(to_learn)['Spanish Word']
-    canvas.itemconfig(card_word, text=new_card)
+    new_card = r.choice(to_learn)
+    span_word = new_card['Spanish Word']
+    canvas.itemconfig(card_word, text=span_word)
     canvas.itemconfig(card_title, text= "Spanish")
+    window.after(3000, flip_card)
+    return new_card
+
+# ---------------------------- UI -------------------------------------------
+
+def flip_card():
+    canvas.create_image(400,263, image = card_back)
+    canvas.itemconfig(card_front, image = card_back)
+    eng_title = canvas.create_text(400,150,text='English', fill= 'white', font=('Ariel', 40, 'italic'))
+    canvas.itemconfig(card_title, text = eng_title)
+    eng_word = canvas.create_text(400,263,text=card['English Translation'], fill='white', font=('Ariel', 40,'bold'))
+    canvas.itemconfig(card_word, text = eng_word)
 
 # ---------------------------- UI -------------------------------------------
 window = Tk() 
@@ -19,8 +33,10 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # -------------------------- Canvas ------------------------------------------
 canvas = Canvas(width=800, height=526, highlightthickness=0,bg=BACKGROUND_COLOR)
-card_pos = PhotoImage(file='images/card_front.png')
-canvas.create_image(400,263,image = card_pos)
+card_front = PhotoImage(file='images/card_front.png')
+card_back = PhotoImage(file='images/card_back.png')
+
+canvas.create_image(400,263,image = card_front)
 card_title = canvas.create_text(400, 150,text='', font=('Ariel', 40,'italic'), fill='black')
 card_word = canvas.create_text(400, 263, text='', font=('Ariel', 60,'bold'), fill='black')
 canvas.grid(column=0, row=0, columnspan=2)
@@ -34,6 +50,6 @@ known_image = PhotoImage(file="images/right.png")
 known_button = Button(image=known_image, borderwidth=0, relief='flat', highlightthickness=0, command=new_word)
 known_button.grid(column=1, row=1)
 
-new_word()
+card = new_word()
 
 window.mainloop()
